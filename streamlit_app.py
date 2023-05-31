11c492ef8a2ccdba93d76d752328eb45f3619262
saurabhjain8 <saurabhjain8@gmail.com>
2023-05-31 17:59:22 +0530

    AWS file import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Some Item 1')
-streamlit.text('Some Item 2')
-streamlit.text('Some Item 3')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
