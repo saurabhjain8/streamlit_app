e74afd0201f7d4688d12f4542eb60c2fb60705ca
saurabhjain8 <saurabhjain8@gmail.com>
2023-05-31 17:46:10 +0530

    menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Some Item 1')
+streamlit.text('Some Item 2')
+streamlit.text('Some Item 3')
